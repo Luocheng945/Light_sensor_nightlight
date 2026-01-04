bf2bca747146f11eba079d9485200ebd33e05701
Luocheng Wu <wuluocheng945@gmail.com>
2026-01-03 23:05:38 -0500

    Add files via upload

---
+++ Code/night_code_1_buffer_time/night_code_1_buffer_time.ino
@@ -0,0 +1,23 @@
+// C++ code
+// NOT TESTED
+//
+void setup()
+{
+    pinMode(A3, INPUT); // Pin 2(analog pin 3) as input
+    pinMode(0, OUTPUT); // Pin 5(PB1) as output
+    pinMode(1, OUTPUT); // Pin 6(PB0) as output
+  
+}
+
+void loop()
+{
+  int r = analogRead(A3); // analog read from analog pin 3
+  if (r > 700) {
+    digitalWrite(0, HIGH);
+    digitalWrite(1, HIGH);
+  } else{
+    digitalWrite(0, LOW);
+    digitalWrite(1, LOW);
+  }
+  delay(500);
+}
